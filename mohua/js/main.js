b0fda47f18226f3c64241350327e28e3ef07af12
Hunter <kamrulhasan25533@gmail.com>
2025-05-02 00:07:28 +0600

    Add files via upload

---
+++ mohua/js/main.js
@@ -0,0 +1,14 @@
+const geneBtn = document.querySelectorAll('.gene-btn')
+
+const showGenerator = () => {
+}
+
+geneBtn.forEach((btn) => {
+  btn.addEventListener('click', () => document.querySelector('.generatorA').classList.add('on'))
+  // console.log('lkfglk')
+})
+console.log(document.querySelector('.main'))
+
+
+
+
